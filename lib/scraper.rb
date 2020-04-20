9e0810542eaf7fb1022a0325850e771de43acfb0
Joseph Kao <josephtkao@gmail.com>
2020-04-20 21:33:33 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,5 +1,6 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
@@ -18,5 +19,5 @@ class Scraper
 
   def get_page
     doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
-
+    binding.pry
 end
